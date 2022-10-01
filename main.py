6a392c34c370c9d05a0a020fffa48688554745eb
Knupwx <wx.perso@gmail.com>
2022-10-01 23:18:56 +0200

    finish

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
 
+a,d = map(int,str(142.7).split("."))
+sign = (a>0)*str(0) or str(1)
+binaire = bin(a)[2:]
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+sn=str(sign)
+expo = bin(127+len(binaire)-1)[2:]
+c = ''
+d= float('0.'+str(d))
+for i in range(23):
+    d = d*2
+    c+=str(d)[0]
+    if int(str(d)[0]) > 0:
+        d = float('0.'+str(d)[-1])
+scientific = binaire[1:]
+res=sn+" "+expo+" "+binaire[1:]+c
+print(res)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+#0 10000111 00000111010011001100110
+#0 10000111 0000011101001100110011001100110
+
