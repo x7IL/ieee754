d4fb22d2c6696eb4f81c2acc379b65bbf5373f25
Knupwx <wx.perso@gmail.com>
2022-10-03 14:57:03 +0200

    finish encode and decode

--- main.py
+++ main.py
@@ -1,25 +1,34 @@
 
 
 
-a,d = map(int,str(324.75).split("."))
-sign = (a>0)*str(0) or str(1)
-binaire = bin(a)[2:]
 
-sn=str(sign)
-expo = bin(127+len(binaire)-1)[2:]
-c = ''
-d= float('0.'+str(d))
-for i in range(23):
-    d = d*2
-    c+=str(d)[0]
-    if int(str(d)[0]) > 0:
-        d = float('0.'+str(d)[-1])
-scientific = binaire[1:]
-res=sn+" "+expo+" "+binaire[1:]+c
-print(res)
+def float_bin(nombre):
 
-#0 10000111 00000111010011001100110
-#0 10000111 0000011101001100110011001100110
+    a,d = map(int,str(nombre).split("."))
+    binaire = bin(a)[2:]
+    sn=str((a>0)*str(0) or str(1))
+    expo = bin(127+len(binaire)-1)[2:]
+    c = ''
 
-#0 10000111 0100010011000000000000000000000
-#0 10000111 01000100110000000000000
+    d = float('0.'+str(d))
+    for i in range(23):
+        d = d*2
+        c+=str(d)[0]
+        if int(str(d)[0]) > 0:
+            d = float('0.'+str(d)[-1])
+
+    return sn+" "+expo+" "+binaire[1:]+c
+def bin_float(bin):
+    tot = 0
+    for i,j in enumerate(bin[1][::-1]):
+        tot += int(j) * 2**(i)
+    exp = tot - 127
+    tot2 = 0
+    for i,j in enumerate(bin[-1]):
+        tot2+= int(j)*2**(-(i+1))
+
+    fin = (-1)**(int(bin[0])) * (1 + tot2) * 2**exp
+    return fin
+
+print(float_bin(432.3))
+print(bin_float(float_bin(432.3).split()))
